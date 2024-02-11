a4e4d321c59f8ece7d7a6db14b1d0d6168d8ea3c
aferuecas <adrianfelipe010494@gmail.com>
2024-02-10 22:23:27 -0500

    Agregamos modificaciones al main.py de manera que ya podemos controlar el filtro por fechas

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import streamlit as st 
 import pandas as pd 
+import numpy as np
+import pydeck as pdk #Debemos importar esta librería
+from streamlit_deckgl import st_deckgl #Debemos importar esta libnrería
+import plotly.express as px
 
 
 #URL del dataset
@@ -12,7 +16,7 @@ st.markdown('Esta aplicación de Streamlit tiene como objetivo mostrar las colis
 #Agregamos este parámetro para que sólo se hace la 'recomputation' cuando hayan cambios
 @st.cache_data(persist=True)
 def load_data(nrows):
-    colisiones = pd.read_csv('colisiones_Cleaned.csv', nrows = nrows)
+    colisiones = pd.read_csv('colisiones_Cleaned.csv', nrows = nrows, parse_dates=['FECHA_HORA_ACC', 'FECHA_OCURRENCIA_ACC'])
     #Nos dimos cuenta también que debemos pasar a minúscula las columnas de latitud y longitud
     #Entonces mejor pasamos todas las columnas a minusculas
     lowercase = lambda x: str(x).lower()
@@ -31,8 +35,25 @@ def load_data2(nrows):
     return colisiones
 
 #Cargamos la data
-data = load_data(20000) #Hagamos la prueba con los primeros 20.000 registros
+data = load_data(2000) #Hagamos la prueba con los primeros 20.000 registros
 #data = load_data2(30000) #Por ahora nos sirve, pero se demora bastante en cargar
+
+# "--------------------------"
+''' 
+try:
+    #Cuántas accidentes hubo  para cada periodo de tiempo
+    st.header('Cuantos accidentes hubo por periodo de tiempo')
+    hour = st.slider('Hora en formato hh:mm ',0, 20)
+    data = data[data['fecha_hora_acc'].dt.hour == hour]
+except:
+    st.write('Aun no hemos podido rsolver el filtro por accidentes y hora')
+
+'''
+# Using filtering options in streamlit to show a specific hour
+st.header("How many collisions occur during a given time of day")
+# A a sidebar st.sidebar.slider
+hour = st.slider("Hour to look at", 0, 23)
+data = data[data['fecha_hora_acc'].dt.hour == hour]
 
 # "--------------------------"
 #Visualicacion de mapa: Se debe hacer con una query
@@ -44,12 +65,68 @@ gravedad_seleccionada = st.select_slider('Seleccione la categoría de gravedad:'
 #Ahora, el mapa
 st.map(data.query("gravedad == @gravedad_seleccionada")[["latitude","longitude"]])
 
+
+
+# "--------------------------"
+'''
+#Usamos pyDeck para configurar la visualización de las layers:
+st.markdown('Colisiones de vehículos efectuadas entre la shoras: ')
+try:
+    midpoint = (np.average(data["latitude"]), np.average(data["longitude"]))
+    deck = pdk.Deck(
+        #map_style="mapbox://styles/mapbox/light-v9",
+        initial_view_state={
+            "latitude": midpoint[0],
+            "longitude": midpoint[1],
+            "zoom": 20,
+            "pitch": 50,
+        },
+        layers=[
+            pdk.Layer(
+                "HexagonLayer",
+                data=data[["fecha_hora_acc", "latitude", "longitude"]]
+            )
+        ]
+    )
+    st.pydeck_chart(deck)
+except Exception as e:
+    st.write(f"Aún no hemos podido resolver el filtro por accidentes y hora. Error: {e}")
+'''
+# ------------------------------------------------------
+st.markdown(f"Vehicle collisions between {hour}:00 and {hour + 1}:00")
+map_midpoint = (np.average(data['latitude']), np.average(data['longitude']))
+
+st.write(pdk.Deck(
+    map_style="mapbox://styles/mapbox/light-v9",
+    initial_view_state={"latitude": map_midpoint[0],
+                        "longitude": map_midpoint[1],
+                        "zoom": 10,
+                        "pitch": 50,
+                        },
+    # see pydeck documentation for more layers
+    layers=[
+        pdk.Layer(
+            "HexagonLayer",
+            data=data[['fecha_hora_acc', 'latitude', 'longitude']],
+            get_position=['longitude', 'latitude'],
+            radius=100,
+            extruded=True,
+            pickable=True,
+            elevation_scale=4,
+            elevation_range=[0, 1000],
+        ),
+    ],
+))
+
+
+
 # "--------------------------"
 #Visualicemos la data en la pap
 #Podemos usar checkbox para que, por default, al usuario le de la opción de clickear si queire ver la data:
 if st.checkbox('Muéstrame la data procesada', False):
     st.subheader('Data Cruda y preprocesada')
     st.write(data)
+
 
 
 
