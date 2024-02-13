d40d43d0b15e20c67e7a0922969969a57d10b783
aferuecas <adrianfelipe010494@gmail.com>
2024-02-12 19:14:48 -0500

    Modificación del archivo main.py. Incluímos un slider para filtrar accidentes por hora y por localidad

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ import pydeck as pdk #Debemos importar esta librería
 from streamlit_deckgl import st_deckgl #Debemos importar esta libnrería
 import plotly.express as px
 
-
+# "-------------------------------------------"
 #URL del dataset
 URL = 'https://datosabiertos.bogota.gov.co/dataset/8624f916-1db2-4c17-b669-19a19b35d1ca/resource/f5862aaa-4e1c-463e-94d5-f04db8164360/download/historico_siniestros_bogota_d.c_-.csv'
 st.title('Registro de siniestros viales en la ciudad de Bogotá, Colombias')
 
-st.markdown('Esta aplicación de Streamlit tiene como objetivo mostrar las colisiones de veh´´ículos en la ciuidad de Bogotá')
-
+st.markdown('Esta aplicación de Streamlit tiene como objetivo mostrar las colisiones de vehículos en la ciudad de Bogotá')
+# "-------------------------------------------"
 #Función para cargar dataset en función del número de registros
 #Agregamos este parámetro para que sólo se hace la 'recomputation' cuando hayan cambios
 @st.cache_data(persist=True)
@@ -33,43 +33,27 @@ def load_data2(nrows):
     colisiones['HORA_ACC'] = colisiones['FECHA_HORA_ACC'].dt.time
 
     return colisiones
-
-#Cargamos la data
-data = load_data(20000) #Hagamos la prueba con los primeros 20.000 registros
-#data = load_data2(30000) #Por ahora nos sirve, pero se demora bastante en cargar
-
-# "--------------------------"
-''' 
-try:
-    #Cuántas accidentes hubo  para cada periodo de tiempo
-    st.header('Cuantos accidentes hubo por periodo de tiempo')
-    hour = st.slider('Hora en formato hh:mm ',0, 20)
-    data = data[data['fecha_hora_acc'].dt.hour == hour]
-except:
-    st.write('Aun no hemos podido rsolver el filtro por accidentes y hora')
-
-'''
-# Using filtering options in streamlit to show a specific hour
-st.header("How many collisions occur during a given time of day")
+# "-------------------------------------------"
+#Cargue de la data
+data = load_data(30000) #Hagamos la prueba con los primeros 20.000 registros
+original_data = data #Creamos esto para, más adelante, poder hacer la selección por localicadaes. Aparentemente la data es modificada abajo
+# "-------------------------------------------"
+# Filtro de accidentes por hora: st.slider()
+st.header("Consulta cuántos accidentes hay dada la diferencia de hora: ")
 # A a sidebar st.sidebar.slider
-hour = st.slider("Hour to look at", 0, 23)
+hour = st.slider("Selecciona la hora entre", 0, 23)
 data = data[data['fecha_hora_acc'].dt.hour == hour]
 
-# "--------------------------"
-#Visualicacion de mapa: Se debe hacer con una query
-st.header('En qué sector de Bogotá está la mayor parte de los heridos por colisiones vehiculares?')
-#con_heridos = st.slider("Numero de personas heridas en colisiones vehiculares",3,9605) #Revisar: el valor maximo de accidentes es 15009
+# "-------------------------------------------"
+#Visualicacion de mapa: uso de st.slider(); st.map() y query
+st.header('Consulta la concentración de accidentes de tránsito por categoría: ')
 # Crear un slider para seleccionar la categoría de gravedad
 gravedad_seleccionada = st.select_slider('Seleccione la categoría de gravedad:',
                                              options=data['gravedad'].unique())
 #Ahora, el mapa
 st.map(data.query("gravedad == @gravedad_seleccionada")[["latitude","longitude"]])
-
-
-
-# "--------------------------"
-
 # ------------------------------------------------------
+#C
 st.markdown(f"Vehicle collisions between {hour}:00 and {hour + 1}:00")
 map_midpoint = (np.average(data['latitude']), np.average(data['longitude']))
 
@@ -95,9 +79,50 @@ st.write(pdk.Deck(
     ],
 ))
 
+# "--------------------------"
 #Creacion de histogramas
-st.subheader('Separación, por minutos, ')
+st.subheader('Separación, por minutos entre  %i:00 and %i:00' % (hour,(hour+1) %24))
+filtered = data[(data['fecha_hora_acc'].dt.hour >= hour) & (data['fecha_hora_acc'].dt.hour < (hour+1))]
+hist = np.histogram(filtered['fecha_hora_acc'].dt.minute, bins=60, range=(0,60))[0]
+chart_data = pd.DataFrame({'minute':range(60), 'crashes':hist})
+fig = px.bar(chart_data, x='minute', y= 'crashes', hover_data = ['minute','crashes'], height=400)
+st.write(fig) #La visualización NO se hace efectiva hasta que haces st.write()
+# "--------------------------"
+#Filtro por Localidad: Cuáles son las localidades con más accidentes?
+st.header('Las 5 localidades con más siniestros vehiculares')
+#Necesitamos conocer los valores únicos de las localidades
+# Crear el diccionario con las condiciones para cada opción
+condiciones = {
+    "choque": "clase_acc = 'choque'",
+    "otro": "clase_acc = 'otro'",
+    "atropello": "clase_acc = 'atropello",
+    "volcamiento": "clase_acc = 'volcamiento'",
+    "caida de ocupante": "clase_acc = 'caida de ocupante'",
+    "autolesion": "clase_acc = 'autolesion'",
+    "incendio": "clase_acc = 'incendio'"
+}
+#--
+# Obtener los valores únicos de la columna CLASE_ACC
+valores_clase_acc = data['clase_acc'].unique()
+# Crear el diccionario con las condiciones para cada opción
+condiciones = {valor: f"clase_acc == '{valor}'" for valor in valores_clase_acc}
 
+#-
+# Obtener la opción seleccionada por el usuario
+opcion_seleccionada = st.selectbox("Selecciona una opción", list(condiciones.keys()))
+# Evaluar la condición correspondiente a la opción seleccionada
+if opcion_seleccionada in condiciones:
+    condicion_evaluar = condiciones[opcion_seleccionada]
+    # Filtrar la DataFrame utilizando la condición
+    df_filtrado = data.query(condicion_evaluar)[['localidad', 'clase_acc']].groupby('localidad').size().reset_index(name='Cantidad de Accidentes')
+    # Ordenar el DataFrame por la columna 'Cantidad de Registros' de forma descendente
+    df_filtrado = df_filtrado.sort_values(by='Cantidad de Accidentes', ascending=False)
+    # Mostrar solo los 10 primeros valores
+    df_filtrado = df_filtrado.head(5)
+    st.write("DataFrame filtrado:")
+    st.write(df_filtrado)
+else:
+    st.write("Opción no válida")
 # "--------------------------"
 #Visualicemos la data en la pap
 #Podemos usar checkbox para que, por default, al usuario le de la opción de clickear si queire ver la data:
